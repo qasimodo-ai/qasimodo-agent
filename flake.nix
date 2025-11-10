8f100dc6a64266637baabc2cff1a9ba0d386473b
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-11-10 21:32:54 +0100

    chore: add formatting

--- flake.nix
+++ flake.nix
@@ -3,23 +3,69 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
     flake-parts.url = "github:hercules-ci/flake-parts";
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "";
+    };
+
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs = {
+        nixpkgs.follows = "";
+        flake-compat.follows = "";
+        gitignore.follows = "";
+      };
+    };
   };
 
-  outputs = inputs @ {flake-parts, ...}:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
 
-      perSystem = {
-        pkgs,
-        ...
-      }: {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            uv
-            python313
-          ];
+      imports = with inputs; [
+        treefmt-nix.flakeModule
+        git-hooks.flakeModule
+      ];
+
+      perSystem =
+        { config, pkgs, ... }:
+        {
+          treefmt = {
+            programs = {
+              nixfmt = {
+                enable = true;
+                width = 120;
+              };
+              nixf-diagnose.enable = true;
+              ruff-format = {
+                enable = true;
+                lineLength = 120;
+              };
+              ruff-check.enable = true;
+              yamlfmt.enable = true;
+            };
+          };
+
+          pre-commit.settings.hooks.treefmt.enable = true;
+
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              uv
+              python313
+            ];
+            shellHook = ''
+              ${config.pre-commit.installationScript}
+            '';
+          };
         };
-      };
     };
 }
