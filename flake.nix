17137d60d45a4e445750f5ec27800bd6c8315cc1
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-11-10 22:01:51 +0100

    chore: build the package with nix

--- flake.nix
+++ flake.nix
@@ -19,6 +19,24 @@
         gitignore.follows = "";
       };
     };
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -37,7 +55,39 @@
       ];
 
       perSystem =
-        { config, pkgs, ... }:
+        {
+          config,
+          pkgs,
+          lib,
+          ...
+        }:
+        let
+          pyproject = lib.importTOML ./pyproject.toml;
+
+          python = pkgs.python313;
+
+          workspaceRoot = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./pyproject.toml
+              ./uv.lock
+              ./src
+            ];
+          };
+
+          workspace = inputs.uv2nix.lib.workspace.loadWorkspace { inherit workspaceRoot; };
+
+          overlay = workspace.mkPyprojectOverlay { sourcePreference = "wheel"; };
+
+          pythonSet = (pkgs.callPackage inputs.pyproject-nix.build.packages { inherit python; }).overrideScope (
+            lib.composeManyExtensions [
+              inputs.pyproject-build-systems.overlays.default
+              overlay
+            ]
+          );
+
+          inherit (pkgs.callPackages inputs.pyproject-nix.build.util { }) mkApplication;
+        in
         {
           treefmt = {
             programs = {
@@ -57,13 +107,29 @@
 
           pre-commit.settings.hooks.treefmt.enable = true;
 
+          packages = {
+            default = config.packages.qasimodo-agent;
+
+            qasimodo-agent = mkApplication {
+              venv = pythonSet.mkVirtualEnv "qasimodo-agent-env" workspace.deps.default;
+              package = pythonSet.qasimodo-agent.overrideAttrs (old: {
+                version = pyproject.project.version;
+                __intentionallyOverridingVersion = true;
+              });
+            };
+          };
+
           devShells.default = pkgs.mkShell {
             packages = with pkgs; [
               uv
-              python313
+              pythonSet.python
+              playwright-driver.browsers
             ];
             shellHook = ''
               ${config.pre-commit.installationScript}
+              export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
+              export PLAYWRIGHT_BROWSERS_PATH="${pkgs.playwright-driver.browsers}"
+              export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
             '';
           };
         };
